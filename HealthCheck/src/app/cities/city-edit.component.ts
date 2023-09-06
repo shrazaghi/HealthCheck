4f34932df326e3f4605bd7c5cee7005acaf0420a
Hosein <hosein@github.com>
2023-09-06 09:27:27 +0330

    Chapter 9: Debug bz ActivityLog

--- HealthCheck/src/app/cities/city-edit.component.ts
+++ HealthCheck/src/app/cities/city-edit.component.ts
@@ -29,6 +29,9 @@ export class CityEditComponent extends BaseFormComponent implements OnInit {
   // the countries array for the select
   countries?: Country[];
 
+  // Activity Log (for debugging purposes)
+  activityLog: string = '';
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
@@ -49,8 +52,36 @@ export class CityEditComponent extends BaseFormComponent implements OnInit {
       countryId: new FormControl('', Validators.required)
     }, null, this.isDupeCity());
 
+
+    // react to form changes
+    this.form.valueChanges
+      .subscribe(() => {
+        if (!this.form.dirty) {
+          this.log("Form Model has been loaded.");
+        }
+        else {
+          this.log("Form was updated by the user.");
+        }
+      });
+
     this.loadData();
+
+    // react to changes in the form.name control
+    this.form.get("name")!.valueChanges
+      .subscribe(() => {
+        if (!this.form.dirty) {
+          this.log("Name has been loaded with initial values.");
+        }
+        else {
+          this.log("Name was updated by the user.");
+        }
+      });
   }
+
+  log(str: string) {
+    this.activityLog += "[" + new Date().toLocaleString() + "] " + str + "<br />";
+  }
+
   loadData() {
     // load countries
     this.loadCountries();
